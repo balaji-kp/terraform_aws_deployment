0cc11ffe88794779419792e427e167153350c18e
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-06 12:04:39 +0530

    Update main.tf

--- fullstack-terraform-AWS-deployment/main.tf
+++ fullstack-terraform-AWS-deployment/main.tf
@@ -11,6 +11,9 @@ module "ec2"{
  source="./ec2"
  webSG=module.vpc.webSG
  web-tier-sub1=module.vpc.web-tier-sub1
+ web-tier-sub2=module.vpc.web-tier-sub2
+ app-tier-sub1=module.vpc.app-tier-sub1
+ app-tier-sub2=module.vpc.app-tier-sub2
 }
 
 
