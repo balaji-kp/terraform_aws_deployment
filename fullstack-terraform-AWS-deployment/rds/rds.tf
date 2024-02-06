21c4fc52c41bd9ccc450534a5a6fd17b87ab9b1d
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-06 15:03:54 +0530

    Update rds.tf

--- fullstack-terraform-AWS-deployment/rds/rds.tf
+++ fullstack-terraform-AWS-deployment/rds/rds.tf
@@ -9,14 +9,52 @@ resource "aws_db_instance" "mysqldb" {
   password             = "password"
   parameter_group_name = "default.mysql5.7"
   vpc_security_group_ids = [aws_security_group.mysqlSG.id]
+  db_subnet_group_name = aws_db_subnet_group.db-subnet-group
   skip_final_snapshot  = true
 }
-resource "aws_security_group" "mysqlSG" {
-  name_prefix = "mysqlSG"
-  ingress {
-    from_port   = 0
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
+resource "aws_db_instance" "my-test-sql" {
+  instance_class          = "db.t3.micro"
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  multi_az                = true
+  storage_type            = "gp2"
+  allocated_storage       = 10
+  name                    = "database1"
+  username                = "admin"
+  password                = "password"
+  apply_immediately       = "true"
+  backup_retention_period = 10
+  backup_window           = "09:46-10:16"
+  db_subnet_group_name    = aws_db_subnet_group.db-subnet-group.name
+  vpc_security_group_ids  = [aws_security_group.db-tier-sg.id]
 }
+
+resource "aws_db_subnet_group" "db-subnet-group" {
+  name       = "db-subnet-group"
+  subnet_ids = [var.db-tier-sub1]
+}
+
+resource "aws_security_group" "db-tier-sg" {
+  name   = "db-tier-sg"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "my-rds-sg-rule" {
+  from_port         = 3306
+  protocol          = "tcp"
+  security_group_id = aws_security_group.db-tier-sg.id
+  to_port           = 3306
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "outbound_rule" {
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = aws_security_group.db-tier-sg.id
+  to_port           = 0
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
