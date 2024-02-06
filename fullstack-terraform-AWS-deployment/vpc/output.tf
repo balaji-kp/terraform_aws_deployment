4cb3ef2cfbf3db243551ce6d488a0e003aa7068e
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-06 11:49:33 +0530

    Update output.tf

--- fullstack-terraform-AWS-deployment/vpc/output.tf
+++ fullstack-terraform-AWS-deployment/vpc/output.tf
@@ -2,8 +2,14 @@ output webSG{
  value = aws_security_group.webSg.id
 }
 output web-tier-sub1{
- value = 
+ value = aws_subnet.web-tier-sub1.id
 }
 output web-tier-sub2{
- value = 
+ value = aws_subnet.web-tier-sub2.id
+}
+output app-tier-sub1{
+ value = aws_subnet.app-tier-sub1.id
+}
+output app-tier-sub2{
+ value = aws_subnet.app-tier-sub2.id
 }
