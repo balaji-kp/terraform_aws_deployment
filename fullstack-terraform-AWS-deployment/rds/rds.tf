58e67e7ee11efd7f9f41d87b99d183a1f3baeb9b
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-06 14:33:25 +0530

    Update and rename rds.tf.txt to rds.tf

--- fullstack-terraform-AWS-deployment/rds/rds.tf
+++ fullstack-terraform-AWS-deployment/rds/rds.tf
@@ -8,5 +8,15 @@ resource "aws_db_instance" "mysqldb" {
   username             = "admin"
   password             = "password"
   parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = [aws_security_group.mysqlSG.id]
   skip_final_snapshot  = true
+}
+resource "aws_security_group" "mysqlSG" {
+  name_prefix = "mysqlSG"
+  ingress {
+    from_port   = 0
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
