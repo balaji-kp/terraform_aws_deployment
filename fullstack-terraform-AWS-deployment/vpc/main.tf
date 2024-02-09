0b59e3d36c0e7ff87df6690e538b060aabf4a716
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-09 19:54:37 +0530

    Update main.tf

--- fullstack-terraform-AWS-deployment/vpc/main.tf
+++ fullstack-terraform-AWS-deployment/vpc/main.tf
@@ -2,6 +2,8 @@
 
 resource "aws_vpc" "myvpc" {
   cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
 }
 
 resource "aws_subnet" "web-tier-sub1" {
@@ -9,8 +11,7 @@ resource "aws_subnet" "web-tier-sub1" {
   cidr_block              = "11.0.0.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
-  enable_dns_hostnames = true
-  enable_dns_support = true
+
 }
 
 resource "aws_subnet" "web-tier-sub2" {
