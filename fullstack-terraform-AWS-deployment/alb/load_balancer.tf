4b7255e533b4c8afb4da95b5f4a38757cf57bb4f
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-10 16:47:52 +0530

    Update load_balancer.tf

--- fullstack-terraform-AWS-deployment/alb/load_balancer.tf
+++ fullstack-terraform-AWS-deployment/alb/load_balancer.tf
@@ -49,3 +49,58 @@ resource "aws_lb_target_group_attachment" "attach2" {
   target_id        = var.web-instance-2
   port             = 80
 }
+
+
+
+
+# creating target group
+resource "aws_lb_target_group" "app-tier-tg" {
+  name     = "app-tier-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc-id
+
+  health_check {
+    path = "/users"
+    port = "traffic-port"
+  }
+}
+
+# creating load balancer
+#create alb
+resource "aws_lb" "app-tier-alb" {
+  name               = "app-tier-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [var.webSG]
+  subnets         = [var.app-tier-sub1, var.app-tier-sub2]
+
+  tags = {
+    Name = "app"
+  }
+}
+
+# attach alb with target group by mention alb listener
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.app-tier-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.app-tier-tg.arn
+    type             = "forward"
+  }
+}
+
+# attach ec2 with target group
+resource "aws_lb_target_group_attachment" "attach3" {
+  target_group_arn = aws_lb_target_group.app-tier-tg.arn
+  target_id        = var.app-instance-1
+  port             = 80
+}
+resource "aws_lb_target_group_attachment" "attach4" {
+  target_group_arn = aws_lb_target_group.app-tier-tg.arn
+  target_id        = var.app-instance-2
+  port             = 80
+}
