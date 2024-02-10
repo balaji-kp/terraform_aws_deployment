ac8c31c697c1a3ae421ec90ad35198c423b19b84
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-10 16:48:21 +0530

    Update variables.tf

--- fullstack-terraform-AWS-deployment/alb/variables.tf
+++ fullstack-terraform-AWS-deployment/alb/variables.tf
@@ -3,5 +3,9 @@ variable "web-tier-sub1"{}
 variable "web-tier-sub2"{}
 variable "web-instance-1"{}
 variable "web-instance-2"{}
+variable "app-tier-sub1"{}
+variable "app-tier-sub2"{}
+variable "app-instance-1"{}
+variable "app-instance-2"{}
 variable "webSG"{}
 
