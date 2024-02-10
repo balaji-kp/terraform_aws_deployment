623b3ebe80031c86dea0e1f1563efdba5ffb355d
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-10 16:49:11 +0530

    Update output.tf

--- fullstack-terraform-AWS-deployment/ec2/output.tf
+++ fullstack-terraform-AWS-deployment/ec2/output.tf
@@ -4,3 +4,9 @@ output web-instance-1{
 output web-instance-2{
  value = aws_instance.webserver2.id
 }
+output app-instance-1{
+ value =aws_instance.appserver1.id
+}
+output app-instance-2{
+ value = aws_instance.appserver2.id
+}
