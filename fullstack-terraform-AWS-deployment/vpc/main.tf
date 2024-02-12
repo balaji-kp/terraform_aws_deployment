458689717e7a8ee251fe4b92cf18fc53ffe2e4bb
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-12 19:05:10 +0530

    changed RT for web-tier

--- fullstack-terraform-AWS-deployment/vpc/main.tf
+++ fullstack-terraform-AWS-deployment/vpc/main.tf
@@ -70,7 +70,7 @@ resource "aws_route_table" "web-tier-RT" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_nat_gateway.NAT_GATEWAY.id
   }
 }
 
