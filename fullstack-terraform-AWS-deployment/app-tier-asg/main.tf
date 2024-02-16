eee997af48263330255d383425f27ceecafa22de
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-16 16:07:34 +0530

    changed

--- fullstack-terraform-AWS-deployment/app-tier-asg/main.tf
+++ fullstack-terraform-AWS-deployment/app-tier-asg/main.tf
@@ -24,8 +24,8 @@ resource "aws_autoscaling_group" "app-tier-ASG" {
   target_group_arns    = ["${var.target_group_arn}"]
   health_check_type    = "ELB"
 
-  min_size = 2
-  max_size = 4
+  min_size = 1
+  max_size = 2
 
   tag {
     key                 = "Name"
