64a7bea39235aed3957b0829b402876d1f0523a3
balaji-kp <77058688+balaji-kp@users.noreply.github.com>
2024-02-18 12:21:23 +0530

    c

--- fullstack-terraform-AWS-deployment/vpc/main.tf
+++ fullstack-terraform-AWS-deployment/vpc/main.tf
@@ -6,6 +6,16 @@ resource "aws_vpc" "myvpc" {
   enable_dns_support = true
 }
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.myvpc.id
+  service_name      = "com.amazonaws.ap-south-1.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids = [aws_route_table.app-tier-RT.id]
+      tags = {
+    Name = "terraform-vpc-s3-endpoint"
+  }
+}
+
 # creat EIP to attach NAT_GATEWAY
 resource "aws_eip" "eip" {
   domain   = "vpc"
